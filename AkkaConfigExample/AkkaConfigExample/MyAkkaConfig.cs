dbcf11da32d47cf1165691518012694f7b757bee
Marin Aglic Cuvic <mrn.aglic@gmail.com>
2021-03-22 09:14:04 +0100

    Added MyAkkaConfig class for configuration
    If we take a look at the appsettings.json file, we will notice that it basically consists of nested json objects. Therefore, we need to reflect that structure in our configuration instance.
    
    That means that the field, for example actor, needs to be mapped to an object that contains the relevant fields. Another example, router needs to be mapped to an object that has the fields for nr-of-instances and router.
    
    For simplicity we define these config sections as nested classes inside our MyAkkaConfig. We could have defined each of these nested classes outside or in a separate file.

---
+++ AkkaConfigExample/AkkaConfigExample/MyAkkaConfig.cs
@@ -0,0 +1,15 @@
+namespace AkkaConfigExample
+{
+    public class MyAkkaConfig
+    {
+        public class ActorConfig
+        {
+            public class DeploymentConfig
+            {
+                public class RouterConfig
+                {
+                }
+            }
+        }
+    }
+}
