fa71aaadba4329b7e692cba5bd407cec2c10e3e4
Marin Aglic Cuvic <mrn.aglic@gmail.com>
2021-03-22 09:19:59 +0100

    Added JsonProperty attributes
    Finally, we need to define how our properties will be serialized/deserialized into json properties.
    We use the JsonProperty attributes and the PropertyName property. The JsonProperty attribute is defined in Newtonsoft.Json.
    The names of the properties defined by PropertyName must match how the fields would be defined in HOCON.

--- AkkaConfigExample/AkkaConfigExample/MyAkkaConfig.cs
+++ AkkaConfigExample/AkkaConfigExample/MyAkkaConfig.cs
@@ -1,17 +1,24 @@
+using Newtonsoft.Json;
+
 namespace AkkaConfigExample
 {
     public class MyAkkaConfig
     {
+        [JsonProperty(PropertyName = "actor")]
         public ActorConfig Actor { get; set; }
         public class ActorConfig
         {
+            [JsonProperty(PropertyName = "deployment")]
             public DeploymentConfig Deployment { get; set; }
             public class DeploymentConfig
             {
+                [JsonProperty(PropertyName = "/my-router")]
                 public RouterConfig MyRouter { get; set; }
                 public class RouterConfig
                 {
+                    [JsonProperty(PropertyName = "router")]
                     public string Router { get; set; }
+                    [JsonProperty(PropertyName = "nr-of-instances")]
                     public int NrOfInstances { get; set; }
                 }
             }
