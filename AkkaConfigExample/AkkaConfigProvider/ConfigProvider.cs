cdc4fb3571c7625b1d2f863cbd11deb416dbd7c7
Marin Aglic Cuvic <mrn.aglic@gmail.com>
2021-03-22 08:24:25 +0100

    Added GetBaseDir method
    We add the GetBaseDir method in order to get the deafult directory. The default directory is the directory of the project that will use our class library called AkkaConfigProvider.

--- AkkaConfigExample/AkkaConfigProvider/ConfigProvider.cs
+++ AkkaConfigExample/AkkaConfigProvider/ConfigProvider.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace AkkaConfigProvider
 {
@@ -13,12 +15,24 @@ namespace AkkaConfigProvider
 
         public ConfigProvider(string filename)
         {
-            
+            string baseDir = GetBaseDir();
         }
 
         public ConfigProvider()
         {
-            
+            string baseDir = GetBaseDir();
+        }
+
+        private string GetBaseDir()
+        {
+            var currDir = Directory.GetCurrentDirectory();
+            var lastIndex = currDir.LastIndexOf("bin", StringComparison.InvariantCultureIgnoreCase);
+            var projectDir =
+                lastIndex < 0
+                    ? currDir
+                    : currDir.Substring(0, currDir.LastIndexOf("bin", StringComparison.InvariantCultureIgnoreCase));
+
+            return projectDir;
         }
     }
 }
