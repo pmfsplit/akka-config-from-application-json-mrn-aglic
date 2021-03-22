5217d4f10f6c3a4965b7735aa1842f4f4e698e9d
Marin Aglic Cuvic <mrn.aglic@gmail.com>
2021-03-22 09:17:23 +0100

    added properties to MyAkkaConfig class and subclasses
    So, the names of the properties defined in our class need to match the names of the fields in appsettings.json. Look at appsettings.json. Under the key "deployment" there is an object whose key is "myrouter". Therefore, our property needs to be called MyRouter. The same is true for the fields in our router object in settings. The properties in our json config file are called router and NrOfInstances. And the same names need to be used in our RouterConfig class.

--- AkkaConfigExample/AkkaConfigExample/MyAkkaConfig.cs
+++ AkkaConfigExample/AkkaConfigExample/MyAkkaConfig.cs
@@ -2,12 +2,17 @@ namespace AkkaConfigExample
 {
     public class MyAkkaConfig
     {
+        public ActorConfig Actor { get; set; }
         public class ActorConfig
         {
+            public DeploymentConfig Deployment { get; set; }
             public class DeploymentConfig
             {
+                public RouterConfig MyRouter { get; set; }
                 public class RouterConfig
                 {
+                    public string Router { get; set; }
+                    public int NrOfInstances { get; set; }
                 }
             }
         }
