36a8230ca11ad100d67880726d8852983b3fbf5f
Marin Aglic Cuvic <mrn.aglic@gmail.com>
2021-03-22 08:22:53 +0100

    Added fields to ConfigProvider
    The IConfiguration interface is located in the package Microsoft.Extensions.Configuration which we added earlier.
    It will hold a Configuration instance.
    The property BaseDir will hold our base directory. It is mostly useful for debugging purposes.

--- AkkaConfigExample/AkkaConfigProvider/ConfigProvider.cs
+++ AkkaConfigExample/AkkaConfigProvider/ConfigProvider.cs
@@ -1,7 +1,11 @@
-﻿namespace AkkaConfigProvider
+﻿using Microsoft.Extensions.Configuration;
+
+namespace AkkaConfigProvider
 {
     class ConfigProvider
     {
+        private readonly IConfiguration _configuration;
+        public string BaseDir { get; }
         public ConfigProvider(string baseDir, string filename)
         {
             
